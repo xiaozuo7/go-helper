b7f0393b2b4ef800bf7b209936642fa85c52daa0
chenpeng <49382984+xiaozuo7@users.noreply.github.com>
2021-11-25 13:53:02 +0800

    Update normal_cs_test.go

--- concurrent/normal_cs_test.go
+++ concurrent/normal_cs_test.go
@@ -7,7 +7,7 @@ import (
 )
 
 func TestConcurrentGet(t *testing.T) {
-	url := "url := \"https://suggest.taobao.com/sug?q=VR&code=utf-8"
+	url := "https://suggest.taobao.com/sug?q=VR&code=utf-8"
 	var stime = time.Now().Unix()
 	NormalCSGet(1000, url)
 	var ctime = time.Now().Unix() - stime
