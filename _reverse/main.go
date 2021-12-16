91c8c46be5456b2ba24920aa30f5994cb1f014a2
chenpeng <49382984+xiaozuo7@users.noreply.github.com>
2021-12-16 15:03:38 +0800

    generics

--- _reverse/main.go
+++ _reverse/main.go
@@ -2,8 +2,12 @@ package main
 
 import "fmt"
 
+type Allow interface {
+	~int | ~int8 | ~int16 | ~int32 | ~int64 | ~string
+}
+
 // Reverse 列表翻转
-func Reverse(obj []any) []any {
+func Reverse[T Allow](obj []T) []T {
 	n := len(obj)
 
 	for i := 0; i < n/2; i++ {
@@ -14,9 +18,11 @@ func Reverse(obj []any) []any {
 	return obj
 }
 
-
-func main(){
-	a := []any{"I", "am", "sb"}
+func main() {
+	a := []string{"I", "am", "sb"}
+	c := []int{1, 2, 3, 4}
 	b := Reverse(a)
-	fmt.Printf("val: %v, type: %T", b, b)
+	fmt.Printf("val: %v, type: %T\n", b, b)
+	fmt.Printf("val: %v, type: %T", c, c)
+
 }
