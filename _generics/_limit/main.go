c92a88a06e14e040e88da534b2e3b74262e15f5d
chenpeng <49382984+xiaozuo7@users.noreply.github.com>
2021-12-27 17:10:44 +0800

    Update main.go

--- _generics/_limit/main.go
+++ _generics/_limit/main.go
@@ -1,9 +1,11 @@
 package main
 
 func main() {
+	Stringify([]string{"1", "2"})
 
 }
 
+// Stringify 调用泛型类型值上的任何方法
 func Stringify[T any](s []T) (ret []string) {
 	for _, v := range s {
 		ret = append(ret, v.String())
