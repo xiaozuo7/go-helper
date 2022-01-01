7186dfa1bedf80aef450d81bff113f3e77434283
chenpeng <49382984+xiaozuo7@users.noreply.github.com>
2022-01-01 18:03:35 +0800

    Update main.go

--- main.go
+++ main.go
@@ -4,16 +4,19 @@ import (
 	"fmt"
 )
 
+// Add int版本的Add
 func Add(a, b int) int {
 	return a + b
 }
 
+// Add float64版本的Add
 func Add(a, b float64) float64 {
 	return a + b
 }
 
 func main() {
-	res1 := Add(1, 2)
-	res2 := Add(1.1, 2.2)
-	fmt.Println(res1, res2)
+	res1 := Add(1, 2)          // 调用int版本Add
+	res2 := Add(1.1, 2.2)      // 调用float64版本的Add
+	res3 := Add("foo" + "bar") // 没有string版本的实现，无法调用
+	fmt.Println(res1, res2, res3)
 }
