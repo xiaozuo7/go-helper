32c8fe8bf156ae03ba1cb88ec99f9a3adef8f086
chenpeng <49382984+xiaozuo7@users.noreply.github.com>
2022-02-18 09:40:56 +0800

    fix

--- main.go
+++ main.go
@@ -1,23 +1,5 @@
 package main
 
-import (
-	"fmt"
-)
-
-type Allow interface {
-	~int | ~float64 | ~string | ~float32
-}
-
-// Add 泛型版本的Add
-func Add[T Allow](a, b T) T {
-	return a + b
-}
-
 func main() {
-	res1 := Add(1, 2)         // 生成int版本Add
-	res2 := Add(1.1, 2.2)     // 生成float64版本的Add
-	res3 := Add("foo", "bar") // 生成string版本Add
-	fmt.Printf("value: %v, type: %T\n", res1, res1)
-	fmt.Printf("value: %.2f, type: %T\n", res2, res2)
-	fmt.Printf("value: %v, type: %T\n", res3, res3)
+	println("Hello, world.")
 }
