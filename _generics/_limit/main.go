463f0c175cd90d6d81af58a07f113ea1853e608e
chenpeng <49382984+xiaozuo7@users.noreply.github.com>
2022-02-28 11:11:47 +0800

    Update main.go

--- _generics/_limit/main.go
+++ _generics/_limit/main.go
@@ -8,7 +8,7 @@ func main() {
 // Stringify 调用泛型类型值上的任何方法
 func Stringify[T any](s []T) (ret []string) {
 	for _, v := range s {
-		ret = append(ret, v.String())
+		ret = append(ret, v.String()) // C++可以，但是Go不行
 	}
 	return
 }
